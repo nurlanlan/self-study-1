e6700a3292d4a7b82fbecad8e316b9b277a83afc
nurlanlan <nurahmad004@gmail.com>
2025-01-18 22:04:18 +0400

    bugs fixed

--- src/main/java/tech/coeus/selfstudyapp/Laptop.java
+++ src/main/java/tech/coeus/selfstudyapp/Laptop.java
@@ -1,8 +1,10 @@
 package tech.coeus.selfstudyapp;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
+@Qualifier("laptop")
 public class Laptop implements Computer {
     public void compile(){
         System.out.println("Laptop chosen");
