ef38ced393bfab3b5e29b34770fb1208a397a351
nurlanlan <nurahmad004@gmail.com>
2025-01-18 21:51:02 +0400

    application context origin

--- src/main/java/tech/coeus/selfstudyapp/SelfStudyAppApplication.java
+++ src/main/java/tech/coeus/selfstudyapp/SelfStudyAppApplication.java
@@ -3,14 +3,15 @@ package tech.coeus.selfstudyapp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SelfStudyAppApplication {
 
     @Autowired
     public static void main(String[] args) {
-        SpringApplication.run(SelfStudyAppApplication.class, args);
-        Dev dev = null;
+        ApplicationContext context = SpringApplication.run(SelfStudyAppApplication.class, args);
+        Dev dev = context.getBean(Dev.class);
         dev.build();
         
     }
